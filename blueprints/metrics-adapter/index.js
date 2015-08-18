174c250189d62a343a47d0f78b84ed630793f779
David Govea <david.govea@runtriz.com>
2015-08-17 17:42:34 -0700

    Fix blueprint typo: "metrics-adapter[s]"

--- blueprints/metrics-adapter/index.js
+++ blueprints/metrics-adapter/index.js
@@ -2,7 +2,7 @@ module.exports = {
   description: 'Generates an ember-metrics adapter.',
 
   locals: function(options) {
-    var importStatement = "import BaseAdapter from 'ember-metrics/metrics-adapter/base';";
+    var importStatement = "import BaseAdapter from 'ember-metrics/metrics-adapters/base';";
     var baseClass = 'BaseAdapter';
     var toStringExtension = 'return ' + "'" + options.entity.name + "';";
     // Return custom template variables here.
