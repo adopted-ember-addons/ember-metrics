75059e9551700a08d1540fa1897162b725d7c614
Lauren Tan <laurent@netflix.com>
2016-08-19 14:37:14 -0700

    Update ember-cli to 2.7.0
    Remove Travis, use CircleCI

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('foo');
 });
 
 export default Router;
