a9c56f17d62d57a6e4a095d704b16e81b042195b
Chris Laubacher <claubacher@gmail.com>
2016-09-13 17:02:36 -0700

    Add tests and clean up dtm adapter

--- tests/unit/metrics-adapters/dynamic-tag-manager-test.js
+++ tests/unit/metrics-adapters/dynamic-tag-manager-test.js
@@ -8,11 +8,41 @@ moduleFor('ember-metrics@metrics-adapter:dynamic-tag-manager', 'dynamic-tag-mana
     config = {
       src: '//path.com/to.src.js'
     };
+    window._satellite = { track() {} };
+  },
+  afterEach() {
+    sandbox.restore();
   }
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var adapter = this.subject({ config });
-  assert.ok(adapter);
+test('#trackEvent calls `_satellite.track`', function(assert) {
+  const adapter = this.subject({ config });
+  const stub = sandbox.stub(window._satellite, 'track');
+
+  const event = 'click-button';
+  const eventData = {
+    event,
+    category: 'button',
+    action: 'click',
+    label: 'nav buttons',
+    value: 4
+  };
+
+  const result = adapter.trackEvent(eventData);
+
+  assert.ok(stub.calledWith(event), 'it sends the right argument');
+  assert.deepEqual(result, eventData, 'it returns the right response');
+});
+
+test('#trackPage calls `_satellite.track`', function(assert) {
+  const adapter = this.subject({ config });
+  const stub = sandbox.stub(window._satellite, 'track');
+
+  const event = 'vpv';
+  const eventData = { event };
+
+  const result = adapter.trackPage();
+
+  assert.ok(stub.calledWith(event), 'it sends the right argument');
+  assert.deepEqual(result, eventData, 'it returns the right response');
 });
