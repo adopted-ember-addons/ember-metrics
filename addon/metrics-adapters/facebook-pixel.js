57ced25e1562e3eed466a77546ff6aff596e988b
Reid Beels <reid@thedyrt.com>
2017-01-15 20:55:26 -0800

    Use https:// scheme for Facebook and GTM adapters
    Using protocol-relative URLs for these adapters fail when the Ember app
    is loaded from a file:// URL, as happens when running as a hybrid
    mobile app. Always using HTTPS for these interactions should be safe
    in all browsers that Ember supports.

--- addon/metrics-adapters/facebook-pixel.js
+++ addon/metrics-adapters/facebook-pixel.js
@@ -28,7 +28,7 @@ export default BaseAdapter.extend({
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
       n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
       t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-      document,'script','//connect.facebook.net/en_US/fbevents.js');
+      document,'script','https://connect.facebook.net/en_US/fbevents.js');
       /* jshint ignore:end */
 
       window.fbq('init', id);
