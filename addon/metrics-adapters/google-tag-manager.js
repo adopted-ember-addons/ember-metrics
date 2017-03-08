f3e845f65c4a097140687f623b0510c537e5e0bf
Reid Beels <mail@reidbeels.com>
2017-03-08 11:16:53 -0800

    Use https:// scheme for Facebook and GTM adapters (#121)
    Using protocol-relative URLs for these adapters fail when the Ember app
    is loaded from a file:// URL, as happens when running as a hybrid
    mobile app. Always using HTTPS for these interactions should be safe
    in all browsers that Ember supports.

--- addon/metrics-adapters/google-tag-manager.js
+++ addon/metrics-adapters/google-tag-manager.js
@@ -37,7 +37,7 @@ export default BaseAdapter.extend({
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script',get(this, 'dataLayer'),id);
       /* jshint ignore:end */
     }
