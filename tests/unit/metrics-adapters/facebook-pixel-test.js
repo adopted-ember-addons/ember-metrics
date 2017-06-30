1fb85b0c128b18d6d6b8457a5a9c68a93f534cfb
Kelly Selden <kellyselden@gmail.com>
2017-06-29 19:47:07 -0700

    fix facebook testing race condition (#151)

--- tests/unit/metrics-adapters/facebook-pixel-test.js
+++ tests/unit/metrics-adapters/facebook-pixel-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 import sinon from 'sinon';
 
@@ -11,15 +12,34 @@ moduleFor('ember-metrics@metrics-adapter:facebook-pixel', 'facebook-pixel adapte
 
     subject = this.subject({ config });
 
-    fbq = sinon.sandbox.stub(window, 'fbq', () => {
-      return true;
-    });
+    return waitForScripts();
   },
 
   afterEach() {
     fbq.restore();
   }
 });
+
+function waitForScripts() {
+  return new Ember.RSVP.Promise(resolve => {
+    (function wait() {
+      // check for the generic script
+      // easiest check to prevent race condition
+      if (window.fbq.version > '2.0') {
+        // now check for the custom script
+        // `fbq.on` is now ready (`fbq.once` would be better but has a bug)
+        window.fbq.on('configLoaded', name => {
+          if (name === config.id) {
+            fbq = sinon.spy(window, 'fbq');
+            resolve();
+          }
+        });
+      } else {
+        Ember.run.later(wait, 10);
+      }
+    })();
+  });
+}
 
 test('#trackEvent calls `fbq.track` with the right arguments', function(assert) {
   subject.trackEvent({ event: 'Foo', opt1: 'bar', opt2: 'baz' });
