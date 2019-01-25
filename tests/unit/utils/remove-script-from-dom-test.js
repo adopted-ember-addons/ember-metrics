9f452acc53459c98fef3750b15e82f30882a6716
Chris Miller <chris.miller@toyotaconnected.com>
2019-01-25 17:00:35 -0600

    back to qunit module

--- tests/unit/utils/remove-script-from-dom-test.js
+++ tests/unit/utils/remove-script-from-dom-test.js
@@ -1,7 +1,7 @@
 import removeScriptFromDOM from '../../../utils/remove-script-from-dom';
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
 
-moduleFor('util:remove-script-from-dom', 'Unit | Util | remove-script-from-dom', {});
+module('Unit | Utility | remove-script-from-dom');
 
 test('it exists', function(assert) {
   assert.ok(removeScriptFromDOM(), 'function can be called');
