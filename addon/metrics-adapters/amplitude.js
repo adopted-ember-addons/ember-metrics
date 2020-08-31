15961286705fa4864cdb0fb5ba363be55d72f4e2
Chris Miller <chris.miller@toyotaconnected.com>
2020-08-31 09:44:00 -0500

    convert to native class

--- addon/metrics-adapters/amplitude.js
+++ addon/metrics-adapters/amplitude.js
@@ -2,18 +2,18 @@ import BaseAdapter from 'ember-metrics/metrics-adapters/base';
 import objectTransforms from 'ember-metrics/utils/object-transforms';
 import removeFromDOM from 'ember-metrics/utils/remove-from-dom';
 import { assert } from '@ember/debug';
-import { get } from '@ember/object';
 import { assign } from '@ember/polyfills';
 
 const { without, compact, isPresent } = objectTransforms;
 
-export default BaseAdapter.extend({
+export default class AmplitudeMetricsAdapter extends BaseAdapter {
   toStringExtension() {
     return 'Amplitude';
-  },
+  }
 
-  init() {
-    const config = get(this, 'config');
+  constructor() {
+    super(...arguments);
+    const { config } = this;
     const { apiKey, options } = config;
 
     assert(
@@ -24,9 +24,9 @@ export default BaseAdapter.extend({
     /* eslint-disable */
     (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
     ;r.type="text/javascript"
-    ;r.integrity="sha384-d/yhnowERvm+7eCU79T/bYjOiMmq4F11ElWYLmt0ktvYEVgqLDazh4+gW9CKMpYW"
+    ;r.integrity="sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
     ;r.crossOrigin="anonymous";r.async=true
-    ;r.src="https://cdn.amplitude.com/libs/amplitude-5.2.2-min.gz.js"
+    ;r.src="https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
     ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
     console.log("[Amplitude] Error: could not load SDK")}}
     ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
@@ -38,19 +38,19 @@ export default BaseAdapter.extend({
     ;return this}
     ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
     ;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
-    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
     ;function v(e){function t(t){e[t]=function(){
     e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
     for(var n=0;n<d.length;n++){t(d[n])}}v(n);
     n.getInstance=function(e){
-    e=(!e||e.length===0?"$default_instance":e).toLowerCase();
-    if(!n._iq) {n._iq = {};}
-    if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]};
-    e.amplitude=n})(window,document);
+    e=(!e||e.length===0?"$default_instance":e).toLowerCase()
+    ;if(!n._iq) {n._iq = {};} // we add this line
+    if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]}
+    ;e.amplitude=n})(window,document);
     /* eslint-enable */
 
     window.amplitude.getInstance().init(apiKey, null, options || {});
-  },
+  }
 
   identify(options = {}) {
     const compactedOptions = compact(options);
@@ -73,7 +73,7 @@ export default BaseAdapter.extend({
 
     window.amplitude.getInstance().identify(identity);
     window.amplitude.getInstance().logEvent('Identify');
-  },
+  }
 
   trackEvent(options = {}) {
     const compactedOptions = compact(options);
@@ -85,26 +85,26 @@ export default BaseAdapter.extend({
     } else {
       window.amplitude.getInstance().logEvent(event);
     }
-  },
+  }
 
   trackPage(options = {}) {
     const eventOpt = { event: 'Page View' };
     const withEvent = assign(eventOpt, options);
 
     this.trackEvent(withEvent);
-  },
+  }
 
   optOut() {
     window.amplitude.getInstance().setOptOut(true);
-  },
+  }
 
   optIn() {
     window.amplitude.getInstance().setOptOut(false);
-  },
+  }
 
   willDestroy() {
     removeFromDOM('script[src*="amplitude"]');
 
     delete window.amplitude;
   }
-});
+}
