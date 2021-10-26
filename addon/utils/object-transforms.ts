a559608bb927a78e260138c92bf89fa94fa30096
Jacob Durham <jfdurham88@gmail.com>
2021-10-26 11:08:55 -0600

    [refactor] `object-transforms` to `.ts`
    - refactors `object-transforms.js` to `.ts`
      - adds `Options` type of `Record<string,string>` to match expected config options object
      - `compact` and `without` wrap new shared `includeKeys` fn
        - were both doing essentially the same thing, returning new config options with subset of given config options based on some check
      - removes unused `only` fn
        - existed only in utils test
      - minor refactor of `isPresent` for single-line check of emptiness
      - adds descriptive jsdoc headers

---
+++ addon/utils/object-transforms.ts
@@ -0,0 +1,52 @@
+import { isPresent as emberIsPresent } from '@ember/utils';
+
+type Options = Record<string, string>;
+
+/**
+ * Present config options
+ * @see {@link https://api.emberjs.com/ember/3.28/functions/@ember%2Futils/isPresent|isPresent}
+ * @param {Options} opts
+ * @returns {Options}
+ */
+export function compact(opts: Options): Options {
+  return includeKeys(opts, (key) => emberIsPresent(opts[key]));
+}
+
+/**
+ * Config options without `excludedKeys`
+ * @param {Options} opts
+ * @param {string[]} excludedKeys
+ * @returns {Options}
+ */
+export function without(opts: Options, excludedKeys: string[]): Options {
+  return includeKeys(opts, (key) => !excludedKeys.includes(key));
+}
+
+/**
+ * Config options with allowed keys
+ * @param {Options} opts
+ * @param {(key:string) => boolean} include
+ * @returns {Options}
+ */
+function includeKeys(
+  opts: Options,
+  include: (key: string) => boolean
+): Options {
+  const newOpts: Options = {};
+
+  for (const key in opts) {
+    if (include(key)) newOpts[key] = opts[key];
+  }
+
+  return newOpts;
+}
+
+export function isPresent(opts: Options): boolean {
+  return Object.getOwnPropertyNames(opts).length > 0;
+}
+
+export default {
+  compact,
+  without,
+  isPresent,
+};
