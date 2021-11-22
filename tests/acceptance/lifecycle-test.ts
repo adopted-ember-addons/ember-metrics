6196976f3e0e3279beb2e7a56004debe29658aa8
Giles Thompson <giles@papaiti.com>
2021-11-22 13:54:04 +1300

    test: add failing lifecycle test

---
+++ tests/acceptance/lifecycle-test.ts
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | lifecycle', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('initialization and teardown in an ember app', async function (assert) {
+    await visit('/');
+    assert.strictEqual(currentURL(), '/');
+  });
+});
