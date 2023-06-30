da63d7e350a9b0074d67a63fd5804b7838f40206
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-06-30 15:12:29 -0400

    Switched from https to http

--- addon/metrics-adapters/matomo-tag-manager.js
+++ addon/metrics-adapters/matomo-tag-manager.js
@@ -23,7 +23,7 @@ export default class MatomoTagManager extends BaseAdapter {
     window._mtm = window._mtm || [];
     window._mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=`https://${matomoUrl}/js/container_${containerId}.js`; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.src=`http://${matomoUrl}/js/container_${containerId}.js`; s.parentNode.insertBefore(g,s);
     g.id='matomo-tag-manager'
   }
 
